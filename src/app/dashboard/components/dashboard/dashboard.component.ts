05299e75f46f12e229bff446abf05802f16d28c4
AAnuj26 <bhagatanuj2828@gmail.com>
2025-02-02 17:26:50 +0530

    Add Breakpoint observer

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: false,
-  
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  GridListColSize: number = 4;
+  GridTileColSize: number = 1;
+  userForm: FormGroup;
 
+  constructor(
+    private readonly BreakpointObserver: BreakpointObserver,
+    private readonly FormBuilder: FormBuilder
+  ) {
+    this.userForm = this.FormBuilder.group({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      age: new FormControl(''),
+      phone: new FormControl(''),
+    });
+
+    this.BreakpointObserver.observe([
+      Breakpoints.Small,
+      Breakpoints.XSmall,
+    ]).subscribe((result) => {
+      console.log('Active Breakpoints:', result.breakpoints);
+      switch (true) {
+        case result.breakpoints[Breakpoints.XSmall]:
+          console.log('Small Screen');
+          this.GridTileColSize = 4;
+          break;
+        case result.breakpoints[Breakpoints.Small]:
+          console.log('Small Screen');
+          this.GridTileColSize = 2;
+          break;
+        default:
+          this.GridTileColSize = 1;
+          break;
+      }
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('Form Values:', this.userForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -6,13 +6,25 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormField, MatHint, MatLabel } from '@angular/material/form-field';
+// import { MatLabel } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInput } from '@angular/material/input';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatGridListModule,
     MatCardModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatIcon,
+    MatHint,
     DashboardRoutingModule,
   ],
 })
